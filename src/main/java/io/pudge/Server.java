7ebb9d8488451c1935cf2f521925d84e19d0b0c5
XIONG Qin <xiongqin@hotmail.com>
2017-02-22 22:19:22 +0100

    poolId + row + column print Server information

--- src/main/java/io/pudge/Server.java
+++ src/main/java/io/pudge/Server.java
@@ -7,10 +7,22 @@ public class Server implements Comparable<Server> {
     private int id;
     private int slotsAccount;
     private int capacity;
+    private int poolId;
+    private int row;
+    private int column;
 
     public Server(int slotsAccount, int capacity) {
         this.slotsAccount = slotsAccount;
         this.capacity = capacity;
+    }
+
+    public void printServer () {
+        System.out.print(row);
+        System.out.print(' ');
+        System.out.print(column);
+        System.out.print(' ');
+        System.out.print(poolId);
+        System.out.println();
     }
 
     public int getId() {
@@ -39,8 +51,8 @@ public class Server implements Comparable<Server> {
 
     @Override
     public int compareTo(Server s){
-        float a = capacity/slotsAccount;
-        float b = s.getCapacity()/s.getSlotsAccount();
+        float a = capacity*1000+slotsAccount;
+        float b = s.getCapacity()*1000+s.getSlotsAccount();
         if(a > b){
             return -1;
         } else if(a == b){
